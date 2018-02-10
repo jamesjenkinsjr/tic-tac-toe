2e60d1244959a9b46c666d2030113d077ae0819b
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-09 19:54:38 -0500

    Install font-awesome, create shell Piece component, create GameGrid file

---
+++ src/Piece.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react.js';
+class Piece extends Component {
+    render() {
+        return (
+            <div>
+                <i alt="piece" aria-hidden="true" onClick={} className={}></i>
+            </div>
+        );
+    }
+
+}
+export default Piece;
