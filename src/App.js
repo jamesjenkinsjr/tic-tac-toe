ff216a4225febfea33f65235fc75c33cb5980514
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-09 21:08:13 -0500

    Get Piece component to render within App, flesh out boxes array data

--- src/App.js
+++ src/App.js
@@ -5,31 +5,68 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      pieces: [
+      boxes: [
         {
-          src: './letter-o.png',
-          isSet: true
+          magicNum: 1,
+          chosen: '',
         },
         {
-          src: './letter-x.png',
-          isSet: true
+          magicNum: 1,
+          chosen: '',
         },
         {
-          src: './letter-x.png',
-          isSet: false
+          magicNum: 1,
+          chosen: '',
+        },
+        {
+          magicNum: 1,
+          chosen: '',
+        },
+        {
+          magicNum: 1,
+          chosen: '',
+        },
+        {
+          magicNum: 1,
+          chosen: '',
+        },
+        {
+          magicNum: 1,
+          chosen: '',
+        },
+        {
+          magicNum: 1,
+          chosen: '',
         }
-
       ]
     }
-
+    this.makeMove = this.makeMove.bind(this);
     
   }
+  makeMove(position) {
+    const clickedSquare = this.state.boxes[position];
+    const first = this.state.boxes.slice(0, position);
+    const last = this.state.boxes.slice(position + 1);
+    const newSquares = [
+      ...first,
+      {...clickedSquare, chosen: !clickedSquare.chosen},
+      ...last
+    ];
+    this.setState({boxes: newSquares});
+
+  }
   render() {
+    const { boxes } = this.state;
+    const GameGrid = boxes.map((box, index) => <Piece 
+      key={index} 
+      /*chosen={this.chosen} 
+      magicNum={this.magicNum}*/
+    />) 
     return (
       <div className="container">
         <h1>Tactical Tick's Toes</h1>
         <div className="row">
-         <Piece />
+          { GameGrid }
         </div>
       </div>
     );
